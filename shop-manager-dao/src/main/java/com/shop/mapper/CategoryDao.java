833bcb61b0e7219b595c85498c0a9b0fda9fdf30
504697121@qq.com <lpy@lvtudiandian.com>
2018-12-24 13:39:34 +0800

    tk.mybatis 分页

---
+++ shop-manager-dao/src/main/java/com/shop/mapper/CategoryDao.java
@@ -0,0 +1,9 @@
+package com.shop.mapper;
+
+import com.shop.pojo.TbItemCat;
+import tk.mybatis.mapper.common.Mapper;
+
+
+public interface CategoryDao extends Mapper<TbItemCat> {
+
+}
--- shop-manager-service/src/main/java/com/shop/service/impl/TbItemCatServiceImpl.java
+++ shop-manager-service/src/main/java/com/shop/service/impl/TbItemCatServiceImpl.java
@@ -1,23 +1,30 @@
 package com.shop.service.impl;
 
 
+import com.github.pagehelper.PageHelper;
+import com.shop.mapper.CategoryDao;
 import com.shop.mapper.TbItemCatMapper;
 import com.shop.pojo.TbItemCat;
 import com.shop.service.TbItemCatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 
 @Service
 public class TbItemCatServiceImpl implements TbItemCatService {
 
-    @Resource
+    @Autowired
     private TbItemCatMapper tbItemCatMapper;
+
+//    @Autowired
+//    private CategoryDao categoryDao;
 
     @Override
     public List<TbItemCat> listItem() {
+        PageHelper.startPage(1,5);
+//        categoryDao.select(null);
         return tbItemCatMapper.listItem();
     }
 }
