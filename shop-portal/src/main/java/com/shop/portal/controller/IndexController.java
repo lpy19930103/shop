399485a1ec129997cf653250a8375225f79df439
504697121@qq.com <lpy@lvtudiandian.com>
2018-12-26 17:50:39 +0800

    门户

---
+++ shop-portal/src/main/java/com/shop/portal/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.shop.portal.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String toIndex() {
+        return "index";
+    }
+}
