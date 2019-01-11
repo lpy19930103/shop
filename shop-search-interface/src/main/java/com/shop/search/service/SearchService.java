9db4942fbf7114dc0bc77a0a138b56efdb3c861f
504697121@qq.com <lpy@lvtudiandian.com>
2019-01-11 11:17:59 +0800

    fix manager bug

---
+++ shop-search-interface/src/main/java/com/shop/search/service/SearchService.java
@@ -0,0 +1,7 @@
+package com.shop.search.service;
+
+import com.shop.common.base.BaseService;
+import com.shop.pojo.TbItem;
+
+public interface SearchService extends BaseService<TbItem> {
+}
---
+++ shop-search-service/src/main/java/com/shop/search/service/impl/SearchServiceImpl.java
@@ -0,0 +1,9 @@
+package com.shop.search.service.impl;
+
+import com.shop.pojo.TbItem;
+import com.shop.search.service.SearchService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchServiceImpl extends BaseServiceImpl<TbItem> implements SearchService {
+}
